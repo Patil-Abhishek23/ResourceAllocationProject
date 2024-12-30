0187b7f5e5df93a210d8471a0d04dc13bbf28840
Patil-Abhishek23 <147656104+Patil-Abhishek23@users.noreply.github.com>
2024-12-31 01:31:17 +0530

    Update EmployeeRepository.java

--- ResourceAllocation/src/main/java/com/estuate/repository/EmployeeRepository.java
+++ ResourceAllocation/src/main/java/com/estuate/repository/EmployeeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.estuate.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByExperienceLessThanEqual(int experience);
+    List<Employee> findByExperienceLessThanEqual(float experience);
 }
